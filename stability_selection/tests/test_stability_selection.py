02dcd73bb4a9b915c8ef9cb87a7e7ca34df114fc
thuijskens <thomas_huijskens@hotmail.com>
2018-03-04 20:53:26 +0000

    Add unit test for regression use case :sparkles:

--- stability_selection/tests/test_stability_selection.py
+++ stability_selection/tests/test_stability_selection.py
@@ -1,8 +1,24 @@
 import numpy as np
 from numpy.testing import assert_almost_equal
 
+from sklearn.linear_model import Lasso
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 from sklearn.utils import check_random_state
-from stability_selection import StabilitySelection, plot_stability_path
+from stability_selection import StabilitySelection
+
+
+def _generate_dummy_regression_data(p=1000, n=1000, k=5, random_state=123321):
+    rng = check_random_state(random_state)
+
+    X = rng.normal(loc=0.0, scale=1.0, size=(n, p))
+    betas = np.zeros(p)
+    important_betas = np.sort(rng.choice(a=np.arange(p), size=k))
+    betas[important_betas] = rng.uniform(size=k)
+
+    y = np.matmul(X, betas)
+
+    return X, y, important_betas
 
 
 def _generate_dummy_classification_data(p=1000, n=1000, k=5, random_state=123321):
@@ -20,7 +36,7 @@ def _generate_dummy_classification_data(p=1000, n=1000, k=5, random_state=123321
     return X, y, important_betas
 
 
-def test_stability_selection():
+def test_stability_selection_classification():
     n, p, k = 1000, 1000, 5
 
     X, y, important_betas = _generate_dummy_classification_data(n=n, k=k)
@@ -30,9 +46,26 @@ def test_stability_selection():
     chosen_betas = selector.get_support(indices=True)
     X_r = selector.transform(X)
 
-    #fig, ax = plot_stability_path(selector)
-
     assert_almost_equal(important_betas, chosen_betas)
     assert(X_r.shape == (n, k))
     assert(selector.stability_scores_.shape == (p, selector.alphas.shape[0]))
 
+
+def test_stability_selection_regression():
+    n, p, k = 1000, 1000, 5
+
+    X, y, important_betas = _generate_dummy_regression_data(n=n, k=k)
+
+    base_estimator = Pipeline([
+        ('scaler', StandardScaler()),
+        ('model', Lasso())
+    ])
+
+    alphas_grid = np.logspace(-1, 4, num=25)
+
+    selector = StabilitySelection(base_estimator=base_estimator, alphas=alphas_grid)
+    selector.fit(X, y)
+
+    chosen_betas = selector.get_support(indices=True)
+
+    assert_almost_equal(important_betas, chosen_betas)
