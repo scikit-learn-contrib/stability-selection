0651c2b13b25e4ef9b758ce29e1e1466df883185
thuijskens <thomas_huijskens@hotmail.com>
2018-03-10 21:42:02 +0000

    Initial commit randomized LASSO :sparkles:

--- stability_selection/__init__.py
+++ stability_selection/__init__.py
@@ -1,3 +1,7 @@
 from .stability_selection import StabilitySelection, plot_stability_path
+from .randomized_lasso import RandomizedLasso, RandomizedLogisticRegression
 
-__all__ = ['StabilitySelection', 'plot_stability_path']
+__all__ = [
+    'StabilitySelection', 'plot_stability_path', 'RandomizedLasso',
+    'RandomizedLogisticRegression'
+]
