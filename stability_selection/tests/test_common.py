2bd62fc30a22507fbf09f2f6a238173340c90e25
thuijskens <thomas_huijskens@hotmail.com>
2018-03-11 13:06:25 +0000

    Add unit tests for StabilitySelection parameter checking.

--- stability_selection/tests/test_common.py
+++ stability_selection/tests/test_common.py
@@ -1,3 +1,5 @@
+from nose.tools import raises
+
 from sklearn.utils.estimator_checks import check_estimator
 from stability_selection import StabilitySelection
 
@@ -7,3 +9,28 @@ def test_transformer():
     # of the positive class
     #return check_estimator(StabilitySelection)
     pass
+
+
+@raises(ValueError)
+def test_check_string_threshold():
+    StabilitySelection(threshold='wrong_value')._validate_input()
+
+
+@raises(ValueError)
+def test_check_threshold_too_large():
+    StabilitySelection(threshold=1.5)._validate_input()
+
+
+@raises(ValueError)
+def test_check_threshold_too_small():
+    StabilitySelection(threshold=0.0)._validate_input()
+
+
+@raises(ValueError)
+def test_check_arguments():
+    StabilitySelection(threshold='wrong_value')._validate_input()
+
+
+@raises(ValueError)
+def test_check_wrong_lambda_name():
+    StabilitySelection(lambda_name='alpha')._validate_input()
