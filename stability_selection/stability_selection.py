be082231450626432fddd0c2081e44e05e6cc143
Fabian Peters <fabian.peters@quantumblack.com>
2018-07-16 13:54:14 +0200

    add stratified sampling

--- stability_selection/stability_selection.py
+++ stability_selection/stability_selection.py
@@ -12,27 +12,28 @@ References
 
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
+from warnings import warn
 
+import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin, clone
 from sklearn.externals.joblib import Parallel, delayed
 from sklearn.feature_selection import SelectFromModel
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
-from sklearn.utils import check_X_y, check_array, safe_mask, check_random_state
+from sklearn.utils import check_array, check_random_state, check_X_y, safe_mask
 from sklearn.utils.validation import check_is_fitted
-from warnings import warn
 
-from .bootstrap import bootstrap_without_replacement, complementary_pairs_bootstrap
-
+from .bootstrap import (bootstrap_without_replacement,
+                        complementary_pairs_bootstrap, stratified_bootstrap)
 
 __all__ = ['StabilitySelection', 'plot_stability_path']
 
 DEFAULT_LAMBDA_GRID = np.logspace(-5, -2, 25)
 BOOTSTRAP_FUNC_MAPPING = {
     'subsample': bootstrap_without_replacement,
-    'complementary_pairs': complementary_pairs_bootstrap
+    'complementary_pairs': complementary_pairs_bootstrap,
+    'stratified': stratified_bootstrap
 }
 
 
